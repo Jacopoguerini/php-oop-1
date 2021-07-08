afc50e8926f10baa53f48ccd367028d1eea0fa68
Jacopoguerini <jacopo.guerini1993@gmail.com>
2021-07-08 16:09:40 +0200

    Inserita funzione

--- index.php
+++ index.php
@@ -15,17 +15,18 @@ metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
         public $year;
         public $plot;
         public $rating;
+        public $reccomandation = "Non valutato";
 
         function __construct($_rating) {
             $this->rating = round($_rating / 2, 1);
         }
 
-        public function isRecommended($reccomandation) {
-            if($rating > 2.5) {
+        public function isRecommended() {
+            if ($this->rating > 2.5) {
                 $this->reccomandation = "Consigliato";
-            } elseif($rating = 2.5) {
+            } elseif ($this->rating = 2.5) {
                 $this->reccomandation = "Non proprio consigliato";
-            } else ($rating < 2.5) {
+            } else {
                 $this->reccomandation = "Non consigliato";
             }
         }
@@ -36,6 +37,7 @@ metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
     $movie1->originalTitle = "The Shawshank Redemption";
     $movie1->year = 1994;
     $movie1->plot = "Andy Dufresne viene condannato, benché innocente, a due ergastoli e al carcere duro. In prigione stringe amicizia con Red, sperimenta la brutalità della vita dietro le sbarre, si adatta, e vive 19 anni sognando la libertà.";
+    $movie1->isRecommended();
     var_dump($movie1);
 
     $movie2 = new Movie(9.2);
@@ -43,6 +45,7 @@ metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
     $movie2->originalTitle = "The Godfather";
     $movie2->year = 1972;
     $movie2->plot = "La vita di Vito Corleone, impegnato nel difficile compito di tenere la famiglia unita e l'impero mafioso compatto.";
+    $movie2->isRecommended();
     var_dump($movie2);
 
     $movie3 = new Movie(9);
@@ -50,6 +53,7 @@ metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
     $movie3->originalTitle = "The Dark Knight";
     $movie3->year = 2008;
     $movie3->plot = "Batman e Jim Gordon si alleano con il nuovo procuratore di Gotham City, Harvey Dent, per combattere il crimine organizzato che dilaga in città e fermare un pericoloso rapinatore, il Joker, che ha gettato la città nell'anarchia.";
+    $movie3->isRecommended();
     var_dump($movie3);
 
     $movie4 = new Movie(8.9);
@@ -57,6 +61,7 @@ metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
     $movie4->originalTitle = "Pulp Fiction";
     $movie4->year = 1994;
     $movie4->plot = "Un killer si innamora della moglie del suo capo, un pugile rinnega la sua promessa e una coppia tenta una rapina che va rapidamente fuori controllo.";
+    $movie4->isRecommended();
     var_dump($movie4);
 
     $movie5 = new Movie(1.9);
@@ -64,6 +69,7 @@ metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
     $movie5->originalTitle = "Disaster Movie";
     $movie5->year = 2008;
     $movie5->plot = "Il film è la parodia dei classici del genere catastrofico degli ultimi anni, a partire dal The Day after Tomorrow per arrivare fino al più recente Hancock.";
+    $movie5->isRecommended();
     var_dump($movie5);
 
     $movie6 = new Movie(3.7);
@@ -71,4 +77,5 @@ metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
     $movie6->originalTitle = "The Room";
     $movie6->year = 2003;
     $movie6->plot = "La storia ruota attorno al triangolo amoroso tra il protagonista Johnny (Tommy Wiseau), Lisa, sua fidanzata e futura moglie, e Mark, il suo migliore amico.";
+    $movie6->isRecommended();
     var_dump($movie6);
